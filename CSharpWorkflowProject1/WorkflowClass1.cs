440316ec6243d831093ff3073ec35d7532425a07
mtcuser <mtcuser@ML-RefVm-30032>
2019-05-30 16:10:35 -0400

    WorkflowActivity

---
+++ CSharpWorkflowProject1/WorkflowClass1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace CSharpWorkflowProject1
+{
+
+
+    public class WorkflowClass1 : CodeActivity
+    {
+
+        [Input("Input")]
+        [Default(null)]
+        [RequiredArgument]
+        public InArgument<String> InputArgument { get; set; }
+
+        [Output("My Output")]
+        public OutArgument<String> OutputArgument { get; set; }
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                var myVariable = executionContext.GetValue(InputArgument);
+
+                myVariable = $"{myVariable}: TEST";
+
+                this.OutputArgument.Set(executionContext, myVariable);
+                //TODO: Do stuff
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
